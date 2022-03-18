406f9287067b1d5bb9912bc99c82bba4bcf512a9
Juan Altmayer Pizzorno <juan@altmayer.com>
2022-03-18 10:10:57 -0400

    - changed message to highlight failure;

--- JustUnit.hxx
+++ JustUnit.hxx
@@ -84,7 +84,7 @@ class TestClass {
         tearDown();
       }
       catch (std::exception& e) { // XXX are we catching too much?
-        std::cerr << m->name << " " << e.what() << "\n";
+        std::cerr << m->name << " FAILED: " << e.what() << "\n";
         tearDown();
       }
     }
